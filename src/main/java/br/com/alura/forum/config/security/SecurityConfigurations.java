6bf4887b6dfa700b15f94af6b916677b4aad22ff
David Trindade <davidt.info@gmail.com>
2022-04-16 09:57:21 -0300

    Autenticando o usuário

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,14 +8,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.alura.forum.security.AutenticacaoService;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private AutenticacaoService autenticacaoService;
 
 	//Configuracoes de atenticacao
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
 	//Configuracoes de Autorizacao
@@ -30,6 +38,10 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	//Configuracoes de recursos estatico(js, css, imagens, etc.)
 	@Override
 	public void configure(WebSecurity web) throws Exception {
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(new BCryptPasswordEncoder().encode("123456"));
 	}
 	
 	
